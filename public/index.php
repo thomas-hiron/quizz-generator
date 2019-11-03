0635c91514cf058a7f3d70bc928541ad09227537
Thomas <thomas.hiron@gmail.com>
2019-11-03 10:45:49 +0100

    Basic controller

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Kernel;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$kernel = new Kernel();
+$kernel->handleRequest();
---
+++ src/Kernel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+class Kernel
+{
+    public function handleRequest()
+    {
+        $requestUri = ltrim($_SERVER['REQUEST_URI'], '/');
+        $requestUriParts = explode('/', $requestUri);
+
+        /* Instanciation du controleur */
+        $controllerNameParts = explode('-', $requestUriParts[0]);
+        $controllerName = "App\\Controller\\".implode('', array_map(function($part) {
+            return ucfirst($part);
+        }, $controllerNameParts));
+
+        if (!class_exists($controllerName)) {
+            http_response_code(404);
+
+            exit;
+        }
+
+        $controller = new $controllerName();
+
+        /* Appel de la méthode, index en fallback */
+        $methodName = $requestUriParts[1] ?? 'index';
+        if (!method_exists($controller, $methodName)) {
+            http_response_code(404);
+
+            exit;
+        }
+
+        call_user_func_array([$controller, $methodName], []);
+    }
+}
